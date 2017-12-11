b20e2de9a6ab7efbe4181676791411ce57404bf8
zhangtaobx <761132807@qq.com>
2017-12-11 15:13:56 +0800

    改变

--- 03/js/script.js
+++ 03/js/script.js
@@ -8,5 +8,14 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    on: {
+        init: function () {
+            swiperAnimateCache(this); //隐藏动画元素 
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function () {
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
 
