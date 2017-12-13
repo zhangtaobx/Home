47f48acd6264fbc63210c1c0070286fee6019b19
zhangtaobx <761132807@qq.com>
2017-12-13 15:25:58 +0800

    改变

---
+++ weichangjing/js/script.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        init: function () {
+            swiperAnimateCache(this); //隐藏动画元素 
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function () {
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    } ,
+    roundLengths : true,
+    effect : 'fade'
+    // cubeEffect: {
+    //     slideShadows: false,
+    //     shadow: false,
+    //     shadowOffset: 1,
+    //     shadowScale: 0
+    // },
+
+});
+var music=document.querySelector("#music");
+var musicBtn=document.querySelector("#musicBtn");
+musicBtn.addEventListener('click',function () {
+    if(music.paused){
+        music.play();
+        musicBtn.style.animationPlayState="running";
+    }else {
+        music.pause();
+        musicBtn.style.animationPlayState="paused";
+    }
+})
+
+
